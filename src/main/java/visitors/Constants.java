9c449ae8227bff80d17742232d4ae08ed0e37591
Domenico <dtropeano@hotmail.it>
2018-01-10 18:33:59 +0100

    more stuff for syntax control

---
+++ src/main/java/visitors/Constants.java
@@ -0,0 +1,12 @@
+package visitors;
+
+import org.omg.CORBA.INTERNAL;
+
+public class Constants {
+    public final static String
+            STRING="string",
+            BOOL="bool",
+            DOUBLE="double",
+            INT="int";
+
+}
--- src/main/java/visitors/SemanticElement.java
+++ src/main/java/visitors/SemanticElement.java
@@ -1,6 +1,9 @@
 package visitors;
 
+import exception.ScopeException;
+
 import java.util.HashMap;
+import java.util.function.BiConsumer;
 
 public class SemanticElement {
     private String data;
@@ -12,6 +15,70 @@ public class SemanticElement {
     public SemanticElement(String data, String type) {
         this.data = data;
         this.type = type;
-        this.canHaveScope=(this.data.equals(NodeConstants.PROGRAMMA));
+        this.canHaveScope=(this.data.equals(NodeConstants.PROGRAM_NODE)||this.data.equals(NodeConstants.PROCDECL_NODE));
+        if(canHaveScope)tos=new HashMap<>();
+    }
+
+    public SemanticElement(String data) {
+        this.data = data;
+    }
+    public boolean isCanHaveScope(){
+        return canHaveScope;
+    }
+    public boolean isCanBeAdded(){
+        return canBeAdded;
+    }
+    public void setCanBeAdded(boolean canBeAdded){
+        this.canBeAdded=canBeAdded;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void addToTos(SemanticElement semanticElement) throws ScopeException {
+        if (tos.put(semanticElement.data, semanticElement) != null)
+            throw new ScopeException(semanticElement.data);
+    }
+
+    public SemanticElement lookup(String data) {
+        return tos.get(data);
+    }
+
+    public void printTos() {
+
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("ToS per " + data);
+
+        tos.forEach(new BiConsumer<String, SemanticElement>() {
+            @Override
+            public void accept(String s, SemanticElement semanticElement) {
+
+                System.out.println(s + ": " + semanticElement);
+                System.out.println();
+            }
+        });
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "SemanticElement{" +
+                "data='" + data + '\'' +
+                ", type='" + type + '\'' +
+                ", canHaveScope=" + canHaveScope +
+                ", canBeAdded=" + canBeAdded +
+                ", tos=" + tos +
+                '}';
     }
 }
